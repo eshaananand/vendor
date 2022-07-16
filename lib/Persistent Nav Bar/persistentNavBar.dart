c8099c780d9c79bc46440a0a5371827e68b8e650
ShashankDeepak <shashankdeepak3@gmail.com>
2022-07-16 16:25:24 +0530

    Vegetable and Fruits page

---
+++ lib/Persistent Nav Bar/persistentNavBar.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+import 'package:vendor_app/screen/homePage.dart';
+import 'package:vendor_app/screen/location_fetch.dart';
+import 'package:vendor_app/screen/login2.dart';
+
+class PersistentNavBar extends StatelessWidget {
+  const PersistentNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PersistentTabController _controller;
+
+    _controller = PersistentTabController(initialIndex: 0);
+
+    // pushNewScreen({required BuildContext context, required Widget screen}){
+
+    // }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.home),
+          title: ("Home"),
+          activeColorPrimary: Colors.red,
+          activeColorSecondary: Colors.white,
+          inactiveColorPrimary: Colors.white,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: ("Settings"),
+          activeColorPrimary: Colors.red,
+          activeColorSecondary: Colors.white,
+          inactiveColorPrimary: Colors.white,
+        ),
+      ];
+    }
+
+    return PersistentTabView(
+      context,
+      backgroundColor: Colors.red,
+      controller: _controller,
+      screens: [
+        homepage(),
+        Location_fetch(),
+      ],
+      items: _navBarsItems(),
+      navBarStyle: NavBarStyle.style9,
+    );
+  }
+}
