0f3b3ff3ffef440d0096e5d9077fbb17bd08587a
ShashankDeepak <shashankdeepak3@gmail.com>
2022-07-19 22:44:53 +0530

    itemsOfShop

--- lib/screen/itemsOfShop.dart
+++ lib/screen/itemsOfShop.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:checkbox_grouped/checkbox_grouped.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
